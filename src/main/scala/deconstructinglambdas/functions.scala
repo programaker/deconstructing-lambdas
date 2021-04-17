1aacd77ade2a7d20358be759910cdd7219165da0
programaker <programaker@gmail.com>
2021-04-17 21:52:21 +0200

    Format

--- src/main/scala/deconstructinglambdas/functions.scala
+++ src/main/scala/deconstructinglambdas/functions.scala
@@ -1,6 +1,8 @@
 import deconstructinglambdas.typeclass.Category
 import Category.given
 
-def thrice[K[_, _]: Category, A](k: K[A, A]): K[A, A] = k >>> k >>> k
+def thrice[K[_, _]: Category, A](k: K[A, A]): K[A, A] = 
+  k >>> k >>> k
 
-def add3: Int => Int = thrice((_: Int) + 1)
+def add3: Int => Int = 
+  thrice(thrice((_: Int) + 1))
